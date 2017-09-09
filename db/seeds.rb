cfe4129c9da701b695c030862782aff6f4c09ec7
peperoncino000 <hm.pudding0715@gmail.com>
2017-09-09 11:11:10 +0900

    Userのダミーデータ作成

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+require 'forgery'
+
+
+100.times do
+  User.create(
+      # name: Forgery(:name).full_name,
+      name: Forgery(:name).full_name,
+      sex: Faker::Number.between(0,1),
+      age: Faker::Number.between(20,25),
+      univ: Faker::Number.between(0,4),
+      grade: Faker::Number.between(1,5)
+  )
+end
