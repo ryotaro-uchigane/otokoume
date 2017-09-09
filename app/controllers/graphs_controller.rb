3456f98759bcf1e71f3fa0e023d04296c56da774
RN <mb@MB.local>
2017-09-09 15:26:01 +0900

    no message

--- app/controllers/graphs_controller.rb
+++ app/controllers/graphs_controller.rb
@@ -2,6 +2,24 @@ class GraphsController < ApplicationController
   def index
   end
   def draw
-    @chart_data = Progress.order('date ASC').group(:date).count
+    if params[:bus].blank? and params[:job].blank?
+      @chart_data0 = Progress.order('date ASC').where("status = 0").group(:date).count
+    else
+      @chart_data0 = Progress.order('date ASC').where("status = 0 and business_category = #{params[:bus]} and occupation_category = #{params[:job]}").group(:date).count
+    end
+
+    if params[:bus].blank? and params[:job].blank?
+      @chart_data1 = Progress.order('date ASC').where("status = 1").group(:date).count
+    else
+      @chart_data1 = Progress.order('date ASC').where("status = 1 and business_category = #{params[:bus]} and occupation_category = #{params[:job]}").group(:date).count
+    end
+
+    if params[:bus].blank? and params[:job].blank?
+      @chart_data2 = Progress.order('date ASC').where("status = 2").group(:date).count
+    else
+      @chart_data2 = Progress.order('date ASC').where("status = 2 and business_category = #{params[:bus]} and occupation_category = #{params[:job]}").group(:date).count
+    end
+
+
   end
 end
