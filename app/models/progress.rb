393f6fe5cdd17db56efa999c7f15ddf01bde77af
peperoncino000 <hm.pudding0715@gmail.com>
2017-09-09 12:12:13 +0900

    Progressのダミーデータの作成

--- app/models/progress.rb
+++ app/models/progress.rb
@@ -1,3 +1,15 @@
 class Progress < ApplicationRecord
   belongs_to :user
+  enum business_category: {
+      it: 0,
+  }
+  enum occupation_category: {
+      programer: 0,
+      comprehensive_work: 1,
+  }
+  enum status: {
+      selection_in: 0,
+      passing: 1,
+      failure: 2,
+  }
 end
