b9add8343980e4da8e6d66c42eb4767a4be55baa
peperoncino000 <hm.pudding0715@gmail.com>
2017-09-09 11:40:19 +0900

    Progressテーブルに日付カラム追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170909004705) do
+ActiveRecord::Schema.define(version: 20170909023624) do
 
   create_table "progresses", force: :cascade do |t|
     t.integer "user_id"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 20170909004705) do
     t.integer "status"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.date "date"
   end
 
   create_table "users", force: :cascade do |t|
