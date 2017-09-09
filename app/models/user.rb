0c326940f1204d96d1be82af0bf482c318bfb753
peperoncino000 <hm.pudding0715@gmail.com>
2017-09-09 11:10:52 +0900

    Userのenum定義

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,11 @@
 class User < ApplicationRecord
   has_many :progresses, dependent: :destroy
+  enum sex: { male: 0, female: 1 }
+  enum univ: {
+      University_of_Oxford: 0,
+      California_Institute_of_Technology: 1,
+      Stanford_University: 2,
+      University_of_Cambridge: 3,
+      Harvard_University: 4,
+  }
 end
