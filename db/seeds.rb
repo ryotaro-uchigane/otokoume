105373ff00d4eb89239849378591fee6fdf10962
peperoncino000 <hm.pudding0715@gmail.com>
2017-09-11 09:54:41 +0900

    seederの修正

--- db/seeds.rb
+++ db/seeds.rb
@@ -44,14 +44,15 @@ end
 end
 
 10.times do
-  BusinessCategory.create(
-      name: Faker::Job.unique.field,
+  job = Faker::Job
+  business_category = BusinessCategory.create(
+      name: job.unique.field,
   )
+  5.times do
+    OccupationCategory.create(
+        business_category_id: business_category.id,
+        name: job.unique.title
+    )
+  end
 end
 
-300.times do
-  OccupationCategory.create(
-      business_category_id: BusinessCategory.where( 'id >= ?', rand(BusinessCategory.first.id..BusinessCategory.last.id) ).first.id,
-      name: Faker::Job.unique.title
-  )
-end
